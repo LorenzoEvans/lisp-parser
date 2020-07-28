f150e7e4eebf9b89c1be4b8c9f4b0316dd336895
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-07-28 14:03:05 -0700

    alright, hier wir gehen

--- src/main.rs
+++ src/main.rs
@@ -11,24 +11,36 @@ use std::collections::BTreeMap;
         // whether or not operators are unary or binary
     // operands
     // whitespace
-    
+
 fn main() {
-    let sourcestr = str::replace("(first (list 1 (+ 2 3) 9))", "(", " ").replace(")", " ");
 
-    let mut ast_map = BTreeMap::new();
+    println!("make_ast output: {:?}",make_ast("(first (list 1 (+ 2 3) 9))".to_string()));
     
-    let ast_tree_1: Vec<&str> = sourcestr.split(" ").collect();
+}
+fn make_ast(source: String) -> BTreeMap<i32, String> {
+    // Remove all the parentheses from the source code as they won't be evaluated.
+    let source_str = str::replace(&source.to_string(), "(", " ").replace(")", " ");
 
-    let mut counter = 0;
-    for i in 0..ast_tree_1.len() {
-        if ast_tree_1[i] != "" {
-            ast_map.insert(counter, ast_tree_1[i]);
-            counter += 1;
+    let mut ast_tree = BTreeMap::new(); // Generated a new ast_map from a BTreeMap to
+                                       // preserve order in which we encounter 'tokens'. 
+    
+    let ast_str: Vec<&str> = source_str.split(" ").collect(); // Split our source string on spaces
+                                                                
+
+    let mut counter = 0; // Create a counter to track the order of our tokens
+                        // as the iterator in the following for loop will contain skips
+                        // from removed spaces and parentheses.
+
+    for i in 0..ast_str.len() { // Create iterator over our string for indexing.
+
+        if ast_str[i] != "" { // We don't want any superfluous spaces.
+
+            ast_tree.insert(counter, ast_str[i].to_string()); // Insert our token into our map, with an int key indicating
+                                                // the tokens 'position'.
+            counter += 1; // Manually increment the counter upon insertion.
         }
     }
 
-    println!("ast_map: {:?}", ast_map);
+    return ast_tree
 
 }
-
-
