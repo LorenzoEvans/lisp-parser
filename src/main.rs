87aed571db040a9b37f19da33cf6264ffdfca303
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-08-26 15:07:52 -0700

    Cleaned up, I love tokenization

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use std::collections::BTreeMap;
+use std::collections::HashMap;
+
+fn main() {
+    println!("{:?}", tokenize(String::from("(first (list 1 (+ 2 3) 9))")))
+}
+
 // We have to parse a string.
     // A string is just a vector of bytes.
 // We want to match certain parts of the string abstractly,
@@ -10,68 +15,114 @@ use std::collections::BTreeMap;
     // operators
         // whether or not operators are unary or binary
     // operands
-    // whitespace
+    // whitespacex
 
-fn main() {
-
-
-    let mut ast = make_ast("(first (list 1 (+ 2 3) 9))".to_string());
-    println!("{:?}", ast);
-    // let obj_vec: Vec<T> = Vec::new(): 
-        // [+ [string [ int ]]]
-      
-
-
-    
+#[derive(Debug,)]
+struct Token{content: (String, usize)} // A token is our smallest building block.
+#[derive(Debug)]
+enum TokenType { // We use an enum, to differentiate between the types of tokens we'll encounter. 
+    Opening(Token),
+    Closing(Token),
+    Char(Token),
+    Operator(Token),
+    Operand(Token),
+    WhiteSpace(Token)
 }
-fn make_ast(source: String) -> BTreeMap<i32, String> {
-    // Remove all the parentheses from the source code as they won't be evaluated.
-    let source_str = str::replace(&source.to_string(), "(", " ");
+fn tokenize(code: String) -> Vec<TokenType> {
 
-    let mut ast_tree = BTreeMap::new(); // Generated a new ast_map from a BTreeMap to
-                                       // preserve order in which we encounter 'tokens'. 
-    
-    let ast_str: Vec<&str> = source_str.split(" ").collect(); // Split our source string on spaces
-                                                                
+    let mut symbol_stack: Vec<TokenType> = Vec::new();
+    let mut ast_string = String::new();
 
-    let mut counter = 0; // Create a counter to track the order of our tokens
-                        // as the iterator in the following for loop will contain skips
-                        // from removed spaces and parentheses.
-
-    for i in 0..ast_str.len() { // Create iterator over our string for indexing.
-
-        if ast_str[i] != "" { // We don't want any superfluous spaces.
-
-            ast_tree.insert(counter, ast_str[i].to_string()); // Insert our token into our map, with an int key indicating
-                                                // the tokens 'position'.
-            counter += 1; // Manually increment the counter upon insertion.
+    for (i, c) in code.char_indices() {
+        match c {
+            '(' => {
+                symbol_stack.push(TokenType::Opening(Token{content:( c.to_string(), i)}));
+                ast_string.push('[');
+            }
+            ')' => {
+                symbol_stack.push(TokenType::Closing(Token{content:( c.to_string(), i)}));
+                ast_string.push(']');
+            }
+            ' ' => {
+                
+                symbol_stack.push(TokenType::WhiteSpace(Token{content:(" ".to_string(), i)}));
+            }
+            '+' => {
+                symbol_stack.push(TokenType::Operator(Token{content:( c.to_string(), i)}));
+                ast_string.push(' ');
+                ast_string.push('+');
+                ast_string.push(' ');
+            }
+            x => { // Using this wildcard to filter out characters and numbers because matching all the utf string characters is highly not feasible.
+                if x.is_numeric() {
+                    symbol_stack.push(TokenType::Operand(Token{content:( c.to_string(), i)}));
+                    ast_string.push(x);
+                }
+                else if c.is_alphabetic() {
+                    symbol_stack.push(TokenType::Char(Token{content:( c.to_string(), i)}));
+                }
+                else {
+                    continue
+                }
+            }
         }
     }
+    symbol_stack
+}
 
-    return ast_tree 
-    // { 0: , 1: "list", 2: "1", 3: "+", 4: "2", 5: "3)", 6: "9))" }
-    // make_ast output: {
-        // 0: "first", 
-            // 1: "list", 2: "1", 
-                // 3: "+", 
-                    // 4: "2", 5: "3", 6: "9"} 
-                    // - here's where the lack of structure costs us.
-                        // 1: "list", is a form that generates a cons list of an arbitrary number of values.
-                        // We don't know, at this point, however, whether or not 9 is a value to be consed
-                        // onto a list, or fed into our `+` operator.
-                        // hack: leave closing parentheses in string, split string values if possible,
-                        // if the second character is a parenthesis, we know a little bit more about nesting
-
-    // How to logically parse this, while missing some structural information, but maintaining order.
-        // The simplest operations to evaluate are primitive operations, on primitive values,
-        // such as `+` and unsigned integers.
-        // For these operations, we need to find the operator, and then grab the next two numbers.
-            //  We can''t exactly assume two, as prefix notation accommodates arbitrary numbers of arguments.\
-        // We can indicate nesting as we know strings are symbols, and symbols indicate beginnings of forms.
-        // "first" indicates one level of nesting
-            // We also know what this function does, so that helps.
-        // "list" indicates another level 
-            // We also know what this function does, so that helps.
-        // We know that + is a function operator, so this also indicates a level of nesting as well.
+fn make_ast(symbol_stack: Vec<TokenType>) -> Vec<(String, usize)> {
+    // Now that we're able to unwrap our enums, 
+    // and push them into a vector, without whitespace or parentheses.
+    // What we need to do now is figure out how to use the index of each character 
+    // to rebuild the ast in order. 
+    //  Generally, we can rely on numerical ordering, even with the gaps, because we don't need to know
+    // what numbers lie between 3 and 9, to know that 9 is greater than 3's, 
+    // or that the distance can't be greater than the number of 3's that can fit in 9.
+        // The distance between integers x and y can never be greater than the number of x's that can fit inside y.
+        // Can we *prove* this, though? *save for later*
+    // We have to take care to cast our operands back to integer values.
+    // # Issue One: We need to generate vectors, for each symbol we encounter.
+        // # We can get away with fixed-arrays, where the expected length is one symbol, and the entire
+        // subsequent nested forms- the issue this brings up is *typing*.
+    // Other than that, we have to figure out looping over our tuples, which increase in order,
+    // allowing us to check for a number skip, due to our missing whitespaces. (This seems shaky, relying on the removal of an empty char)
+    
+    let mut ast: Vec<(String, usize)> = Vec::new();
+    for i in symbol_stack {
         
+        match i {
+            TokenType::Opening(Token { content }) => {
+                continue 
+            }
+            TokenType::Char(Token { content }) => {
+                ast.push((String::from(content.0), content.1))
+            }
+            TokenType::Operator(Token { content }) => {
+                ast.push((String::from(content.0), content.1))
+            }
+            TokenType::Operand(Token { content }) => {
+                ast.push((String::from(content.0), content.1))
+            }
+            _ => {
+                continue
+            }
+        }
+    }
+    ast
+
+}
+fn output_ast(ast: Vec<(String, usize)>) -> Vec<String> {
+
+    let mut cur_idx = 0; // manual iterator for peeking forward
+    let mut s_tree = vec![];
+    for i in 0..ast.len() - 1 { // take the length so that our iterator is the item
+        if (ast[i + 1].1 - ast[i].1) > 1 { // if the next items second value minus the prev items second value
+                                          // is greater than 1, we've hit a skip in our string count and finished a symbol.
+
+        }
+        cur_idx = ast[i].1;
+        s_tree.push(ast[i].0.clone());
+    }
+    s_tree.push(ast[ast.len() - 1].0.clone());
+    s_tree
 }
---
+++ src/maindepr.rs
@@ -0,0 +1,66 @@
+// use std::collections::BTreeMap;
+// // We have to parse a string.
+//     // A string is just a vector of bytes.
+// // We want to match certain parts of the string abstractly,
+// // and capture them in a nested format.
+// // Alright so we can capture individual chars, as well as whitespace,
+// // in terms of the Lisp code, we need to differentiate between:
+//     // delimiters
+//     // symbols
+//     // operators
+//         // whether or not operators are unary or binary
+//     // operands
+//     // whitespace
+
+
+// fn make_ast(source: String) -> BTreeMap<i32, String> {
+//     // Remove all the parentheses from the source code as they won't be evaluated.
+//     let source_str = str::replace(&source.to_string(), "(", " ");
+
+//     let mut ast_tree = BTreeMap::new(); // Generated a new ast_map from a BTreeMap to
+//                                        // preserve order in which we encounter 'tokens'. 
+    
+//     let ast_str: Vec<&str> = source_str.split(" ").collect(); // Split our source string on spaces
+                                                                
+
+//     let mut counter = 0; // Create a counter to track the order of our tokens
+//                         // as the iterator in the following for loop will contain skips
+//                         // from removed spaces and parentheses.
+
+//     for i in 0..ast_str.len() { // Create iterator over our string for indexing.
+
+//         if ast_str[i] != "" { // We don't want any superfluous spaces.
+
+//             ast_tree.insert(counter, ast_str[i].to_string()); // Insert our token into our map, with an int key indicating
+//                                                 // the tokens 'position'.
+//             counter += 1; // Manually increment the counter upon insertion.
+//         }
+//     }
+
+//     return ast_tree 
+//     // { 0: , 1: "list", 2: "1", 3: "+", 4: "2", 5: "3)", 6: "9))" }
+//     // make_ast output: {
+//         // 0: "first", 
+//             // 1: "list", 2: "1", 
+//                 // 3: "+", 
+//                     // 4: "2", 5: "3", 6: "9"} 
+//                     // - here's where the lack of structure costs us.
+//                         // 1: "list", is a form that generates a cons list of an arbitrary number of values.
+//                         // We don't know, at this point, however, whether or not 9 is a value to be consed
+//                         // onto a list, or fed into our `+` operator.
+//                         // hack: leave closing parentheses in string, split string values if possible,
+//                         // if the second character is a parenthesis, we know a little bit more about nesting
+
+//     // How to logically parse this, while missing some structural information, but maintaining order.
+//         // The simplest operations to evaluate are primitive operations, on primitive values,
+//         // such as `+` and unsigned integers.
+//         // For these operations, we need to find the operator, and then grab the next two numbers.
+//             //  We can''t exactly assume two, as prefix notation accommodates arbitrary numbers of arguments.\
+//         // We can indicate nesting as we know strings are symbols, and symbols indicate beginnings of forms.
+//         // "first" indicates one level of nesting
+//             // We also know what this function does, so that helps.
+//         // "list" indicates another level 
+//             // We also know what this function does, so that helps.
+//         // We know that + is a function operator, so this also indicates a level of nesting as well.
+        
+// }
